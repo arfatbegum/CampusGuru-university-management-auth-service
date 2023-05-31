927527f1a80c6cce46e7b1b66322a904dcaaf14c
arfat132 <arfatbegum90@gmail.com>
2023-05-31 21:06:48 +0300

    Setup ESlint, prettier, husky and lint staged

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,13 @@
-import express, { Application, Request, Response } from "express";
-import cors from "cors";
-const app: Application = express();
-const port = 3000;
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+const app: Application = express()
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Sever is running");
-});
+app.get('/', (req: Request, res: Response) => {
+  res.send('Server is running')
+})
 
-export default app;
+export default app
