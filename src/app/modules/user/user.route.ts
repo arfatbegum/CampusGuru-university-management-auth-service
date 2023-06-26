be68a30f550ffd157b83ac6023f01f85e5eb4109
arfat132 <arfatbegum90@gmail.com>
2023-06-26 12:13:51 +0300

    [DONE] Faculty module

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -8,6 +8,11 @@ router.post(
   '/create-student',
   validateRequest(UserValidation.createUserZodSchema),
   UserController.createStudent
+); 
+router.post(
+  '/create-faculty',
+  validateRequest(UserValidation.createFacultyZodSchema),
+  UserController.createFaculty
 );
 
 export const UserRoutes = router;
