ab6d22a5ad164e6cd5e5c5d7e53efc16f585fb7b
arfat132 <arfatbegum90@gmail.com>
2023-07-08 00:56:37 +0300

    [DONE] admin module

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -8,11 +8,16 @@ router.post(
   '/create-student',
   validateRequest(UserValidation.createUserZodSchema),
   UserController.createStudent
-); 
+);
 router.post(
   '/create-faculty',
   validateRequest(UserValidation.createFacultyZodSchema),
   UserController.createFaculty
+);
+router.post(
+  '/create-admin',
+  validateRequest(UserValidation.createAdminZodSchema),
+  UserController.createAdmin
 );
 
 export const UserRoutes = router;
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -5,6 +5,8 @@ import { AcademicFacultyRoutes } from '../modules/academicFaculty/academicFacult
 import { AcademicDepartmentRoutes } from '../modules/academicDepartment/academicDepartment.route';
 import { StudentRoutes } from '../modules/student/student.route';
 import { FacultyRoutes } from '../modules/faculty/faculty.route';
+import { AdminRoutes } from '../modules/admin/admin.route';
+import { ManagementDepartmentRoutes } from '../modules/managementDepartment/managementDepartment.route';
 
 const router = express.Router();
 
@@ -26,12 +28,20 @@ const moduleRoutes = [
     route: AcademicDepartmentRoutes,
   },
   {
+    path: '/management-departments',
+    route: ManagementDepartmentRoutes,
+  },
+  {
     path: '/students',
     route: StudentRoutes,
   },
   {
     path: '/faculties',
     route: FacultyRoutes,
+  },
+  {
+    path: '/admins',
+    route: AdminRoutes,
   },
 ];
 
