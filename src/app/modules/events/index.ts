3321a55c34cfb80d1065f82de82bc708d704eb8c
arfat132 <arfatbegum90@gmail.com>
2023-09-25 20:53:21 +0300

    [done] add redis in Academic departments

--- src/app/modules/events/index.ts
+++ src/app/modules/events/index.ts
@@ -1,7 +1,9 @@
+import initAcademicDepartmentEvents from "../academicDepartment/academicDepartment.event";
 import initAcademicSemesterEvents from "../academicSemeter/academicSemester.event";
 
 const subscribeToEvents = () => {
     initAcademicSemesterEvents();
+    initAcademicDepartmentEvents();
 }
 
 export default subscribeToEvents;
